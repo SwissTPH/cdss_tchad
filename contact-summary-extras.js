f7f7e87907a91e80c103a8be31bd120f24c376e9
delcroip <patrick.delcroix@swisstph.ch>
2023-04-25 10:49:22 +0200

    misc

--- contact-summary-extras.js
+++ contact-summary-extras.js
@@ -3,7 +3,8 @@ var now = Date.now();
 var pregnancyForms = [
 ];
 
-var antenatalForms = [
+var CPN_FORMS = [
+  'cpn'
 ];
 
 var deliveryForms = [
@@ -32,7 +33,76 @@ var IMMUNIZATION_WOMEN_LIST = [
   'vat',
 ];
 
-
+var CPN_CASE_DATA = [
+'p_faf',
+'p_moustiquaire',
+'p_tpi',
+'p_deparasitage',
+'p_conseils',
+'p_hygiene',
+'p_info',
+'p_ptme',
+'p_vih',
+'p_autre_mesr',
+'s_saignement',
+'s_leucorrhee',
+'s_perte',
+'s_cephale',
+'s_convulsion',
+'s_respiration',
+'s_essoufflement',
+'s_fievre',
+'s_douleur',
+'s_mains',
+'s_toux',
+'s_pertes_vaginales',
+'s_contraction_uterine',
+'s_rupture_poche',
+'s_lieu_accouche',
+'lieu_accouche',
+'s_moyen_transport',
+'moyen_transport',
+'s_economie',
+'s_preneurs_decisions',
+'preneurs_decisions',
+'s_donneurs_sang',
+'donneurs_sang',
+'s_id_accompagnate',
+'id_accompagnate_note',
+'s_necessaires',
+'diabete',
+'drepanocytose',
+'cardiopathie',
+'hypertension_ateriel',
+'tuberculose',
+'autre',
+'sign_antecedent',
+'p_int_cpn',
+'date_cpn_1',
+'missed_cpn_1',
+'date_cpn_2',
+'missed_cpn_2',
+'date_cpn_3',
+'missed_cpn_3',
+'date_cpn_4',
+'missed_cpn_4',
+'date_cpn_5',
+'missed_cpn_5',
+'date_cpn_6',
+'missed_cpn_6',
+'date_cpn_7',
+'missed_cpn_7',
+'date_cpn_8',
+'missed_cpn_8',
+'r_test_grossesse',
+'t_test_grossesse',
+'affi_method',
+'ddr_date',
+'dpa_date',
+'no_info_pregnancy_reason',
+'age_estima',
+'label_esti',
+];
 var MS_IN_DAY = 24*60*60*1000;  // 1 day in ms
 var MAX_DAYS_IN_PREGNANCY = 44*7;  // 44 weeks
 var DAYS_IN_PNC = 45; // Allow for 3 extra days to receive PNC reports
@@ -117,7 +187,7 @@ function hasValue(path){
 
 
 function isDate(date_str) {
-  return (new Date(date_str) !== 'Invalid Date') && !isNaN(new Date(date_str));
+  return (date_str !== undefined && date_str!==null && new Date(date_str) !== 'Invalid Date') && !isNaN(new Date(date_str));
 }
 
 
@@ -189,7 +259,22 @@ function initImmunizations(allReports, doses_list, forms_list) {
 const initChildImmunizations = function(allReports) {
   return initImmunizations(allReports, IMMUNIZATION_CHILD_DOSES, IMMUNIZATION_CHILD_FORMS);
 };*/
+function initDatas(allReports, datas_list, forms_list) {
+  var master = {};
+  datas_list.forEach(function(data) {
+    master[data[0]] = null;
+  });
+  var i;
+  // loop over all reports
+  for(i=0; i<allReports.length; ++i) {
+    const report = allReports[i];
+    if(report && forms_list.includes(report.form) && report.fields){
+        master = addDatas(master,  report.fields, datas_list);
+    }
 
+  }
+  return master;
+}
 
 
 function getAgeInYears() {
@@ -201,7 +286,19 @@ function getAgeInYears() {
   }
 }
 
+function dateFormat(datestr, iso=false){
+  let objectDate = new Date(datestr);
+  let day = objectDate.getDate();
+  let month = objectDate.getMonth()+1;
+  let year = objectDate.getFullYear();
+  if (iso){
+    return `${year}-${month}-${day}`;
 
+  }else{
+    return `${day}/${month}/${year}`;
+
+  }
+}
 
 function getImmFileds(allReports, list_doses, list_forms, list_imm) {
   const fields = [];
@@ -214,14 +311,15 @@ function getImmFileds(allReports, list_doses, list_forms, list_imm) {
     };
     if (isSingleDose(imm,list_doses)) {
 
-      field.value = hasValue(immunizations[imm]) && isDate(immunizations[imm]) ?  new Date(immunizations[imm]) : null;
+      field.value = hasValue(immunizations[imm]) && isDate(immunizations[imm]) ?  dateFormat(immunizations[imm]) : null;
     } else {
-      field.value = 'contact.imm.doses';
-      field.context = {
-        count: countDosesReceived(immunizations, imm, list_doses),
-        total: countDosesPossible(imm, list_doses),
-        last: getMaxDose(immunizations, imm)
-      };
+
+      let dose_recieved  = countDosesReceived(immunizations, imm, list_doses);
+      field.value = 'faite: '+countDosesReceived(immunizations, imm, list_doses)
+        + '\nattendue: '+countDosesPossible(imm, list_doses);
+      if (dose_recieved>0) field.value += '\ndernière: '+dateFormat(getMaxDose(immunizations, imm));
+
+      
     }
     fields.push(field);
   });
@@ -235,25 +333,42 @@ const getImmWomenFileds = function (allReports) { return getImmFileds(allReports
 const allReports = reports;
 
 
-function generateContext(ctx,list_doses, list_forms){
+function generateImmContext(ctx,list_doses, list_forms){
   var immunizations = initImmunizations(allReports, list_doses, list_forms);
   // add the entry in the context only if there is a value
   Object.entries(immunizations).forEach(([key, value]) => {
       
       if (value !== null ){
-        //console.log('imm_date_'+key+':'+value);
-        ctx['imm_date_'+key]=value;
+          //console.log('imm_date_'+key+':'+value+dateFormat(value));
+        ctx['imm_date_'+key]=isDate(value) ? dateFormat(value, true):value;
+      }
+  });
+}
+
+
+
+function generateDataContext(ctx, prefix,list_data, list_forms){
+  var case_datas = initDatas(allReports, list_data, list_forms);
+  console.log(case_datas.length);
+  // add the entry in the context only if there is a value
+  Object.entries(case_datas).forEach(([key, value]) => {
+      if (value !== null ){
+        console.log(prefix+key+':'+value);
+        ctx[prefix+key]=value;
       }
   });
 }
 
 var generateWomenContext =  function (ctx){
-  generateContext(ctx,IMMUNIZATION_WOMEN_DOSES, IMMUNIZATION_WOMEN_FORMS );
+  generateImmContext(ctx,IMMUNIZATION_WOMEN_DOSES, IMMUNIZATION_WOMEN_FORMS );
+  generateDataContext(ctx,'cpn_',CPN_CASE_DATA, CPN_FORMS );
+  
 };
 
 
-var generateChlidContext =  function (ctx){
-  generateContext(ctx,IMMUNIZATION_CHILD_DOSES, IMMUNIZATION_CHILD_FORMS );
+var generateChildContext =  function (ctx){
+  generateImmContext(ctx,IMMUNIZATION_CHILD_DOSES, IMMUNIZATION_CHILD_FORMS );
+
 };
 
 
@@ -263,26 +378,48 @@ var generateChlidContext =  function (ctx){
 var addImmunizations = function(master, report_path, doses_list) {
   doses_list.forEach(function(dose) {
     if(!master[dose[0]] && hasValue(report_path['date_' + dose[0]])) {
-      master[dose[0]] = report_path['date_' + dose[0]];
+      master[dose[0]] = Date.parse(report_path['date_' + dose[0]]);
     }
   });
 };
+
+
+// add the date only if found on the master and has value in report_path
+
+function addDatas(result, nodes, datas_list){
+  for (const [key, value] of Object.entries(nodes)) {
+    if(isNaN(key) && value !== null && value !== undefined && key !== 'meta' && key !== undefined ){
+      console.log('read'+':'+key+':'+(typeof value)+':'+value);
+      if( typeof value === 'object' && Object.getOwnPropertyNames(value).length > 0){
+          console.log('dd'+':'+key+':'+(typeof value)+':'+value);
+          result = addDatas(result,value,datas_list);
+      }else if (key.trim() in datas_list || key === 'p_int_cpn'){
+        console.log('add'+':'+key+':'+(typeof value)+':'+value);
+        result[key]=(value);
+      }
+    }
+  }
+  return result;
+}
+
 // count the number of time there is name_ in the "master" list with a value
 function getMaxDose(master, name) {
     var filtered = getImmSubList(master, name);
-    return filtered.length >0 ? new Date(Math.max.apply(null,filtered)): null;
+    return Object.keys(filtered).length >0 ? (Math.max.apply(null,
+      Object.keys(filtered).map(function(key){return filtered[key];})
+    )): null;
 }
 
 // count the number of time there is name_ in the "master" list with a value
 var countDosesReceived = function(master, name, doses_list ) {
   return count(doses_list, function(dose) {
-    return master[dose[0]] && dose[0].indexOf(name + '_') === 0;
+    return master[dose[0]] && dose[0].indexOf(name) === 0;
   });
 };
 // count the number of time there is name_ in the "master" list 
 function countDosesPossible(name, doses_list) {
   return count(doses_list, function(dose) {
-    return dose[0].indexOf(name + '_') === 0;
+    return dose[0].indexOf(name ) === 0;
   });
 }
 
@@ -294,27 +431,19 @@ var isSingleDose = function(name, doses_list) {
 };
 
 function getImmSubList(master, name) {
-  return Object.keys(master)
-  .filter( key => key.includes('date_' + name))
-  .reduce((obj, key) => {
-    obj[key] = isDate(master[key]) ? new Date(master[key]):null;
-    return obj;
-  }, {});
-
-
+  return Object.fromEntries(Object.entries(master).filter(([key, value]) => key.indexOf(name) === 0 && isDate(value)));
 }
 
 module.exports = {
   now,
   pregnancyForms,
-  antenatalForms,
   deliveryForms,
   postnatalForms,
   MS_IN_DAY,
   MAX_DAYS_IN_PREGNANCY,
   DAYS_IN_PNC,
   generateWomenContext,
-  generateChlidContext,
+  generateChildContext,
   count,
   isFacilityDelivery,
   isNonFacilityDelivery,
@@ -326,7 +455,7 @@ module.exports = {
   isAlive,
   getOldestReport,
   getAgeInMonths,
-  generateContext,
+  generateImmContext,
   getAgeInYears,
   getImmChildFileds,
   getImmWomenFileds,
